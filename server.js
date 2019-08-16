85cb36fafb495fbf5b80e81f90a93860599a6030
Zacharey Carey <zac4ncstate@aol.com>
2019-08-15 22:35:20 -0400

    fixes

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 var express = require("express");
-var bodyParser = require("bodyparser");
+var bodyParser = require("body-parser");
 var exphbrs = require("express-handlebars");
 
-var PORT = process.env.PORT 8889;
+var PORT = process.env.PORT || 8889;
 var app = express();
 
 app.use(express.static("public"));
 
-app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(bodyParser.json());
 
-app.engine("handlebars", exphbrs({ defaultLayout: "main"}));
+app.engine("handlebars", exphbrs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 var routes = require("./controllers/burgers_controllers.js");
@@ -19,14 +19,5 @@ var routes = require("./controllers/burgers_controllers.js");
 app.use(routes);
 
 app.listen(PORT, function() {
-    console.log("Listening to port:", PORT);
+  console.log("Listening to port:", PORT);
 });
-
-
-
-
-
-
-
-
-
