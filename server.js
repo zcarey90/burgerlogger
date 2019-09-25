6b9435db35e8f16fd37927d7ed50f0ca9151d5b1
Zacharey Carey <zac4ncstate@aol.com>
2019-09-25 14:46:02 -0400

    clean up

--- server.js
+++ server.js
@@ -2,8 +2,31 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var exphbrs = require("express-handlebars");
 
-var PORT = process.env.PORT || 8889;
 var app = express();
+
+app.engine("handlebars", exphbrs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var PORT = 3000;
+app.listen(port);
+
+var burgers = [
+  { name: "hamburger", price10, awesomeness: 4 },
+  { name: "cheeseburger", price5, awesomeness: 8 },
+  { name: "baconburger", price3, awesomeness: 5 }
+];
+
+app.get("/", function(req, res) {
+  res.render("burgers", { brgrs: burgers });
+});
+
+app.get("/:name", function(req, res) {
+  for (var i = 0; i < burgers.length; i++) {
+    if (burgers[i].name == req.params.name) {
+      return res.render("burger", burgers[i]);
+    }
+  }
+});
 
 app.use(express.static("public"));
 
